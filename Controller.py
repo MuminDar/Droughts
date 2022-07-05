ba1f20c2ee090fe41017db5f013b2ea3234138a9
Mumin Dar <108408004+MuminDar@users.noreply.github.com>
2022-07-05 09:12:44 +0000

    Bug fixes

--- Controller.py
+++ Controller.py
@@ -23,62 +23,68 @@ def ValidateMove(CurrentX,CurrentY,Direction):
     print('Incorrect Move: Not a Piece')
   else:
     if board[CurrentX][CurrentY].PieceColour == 'b' or board[CurrentX][CurrentY].queen == True:
-      if Direction == 'UpLeft':
-        if board[CurrentX-1][CurrentY-1] == 0:
-          return(True)
-        else:
-          print("Invalid move")
-      if Direction == 'UpRight':
-        if board[CurrentX-1][CurrentY+1] == 0:
-          return(True)
-        else:
-          print("Invalid move")
+      if CurrentY != 0 and CurrentX != 0:
+        if Direction == 'UpLeft':
+          if board[CurrentX-1][CurrentY-1] == 0:
+            return(True)
+      if CurrentY != 9 and CurrentX != 0:
+        if Direction == 'UpRight':
+          if board[CurrentX-1][CurrentY+1] == 0:
+            return(True)
     if board[CurrentX][CurrentY].PieceColour == 'w' or board[CurrentX][CurrentY].queen == True:
-      if Direction == 'DownLeft':
-        if board[CurrentX+1][CurrentY-1] == 0:
-          return(True)
-        else:
-          print("Invalid move")
-      if Direction == 'DownRight':
-        if board[CurrentX+1][CurrentY+1] == 0:
-          return(True)
-        else:
-          print("Invalid move")
+      if CurrentY != 0 and CurrentX != 9:
+        if Direction == 'DownLeft':
+          if board[CurrentX+1][CurrentY-1] == 0:
+            return(True)
+      if CurrentY != 9 and CurrentX != 9:
+        if Direction == 'DownRight':
+          if board[CurrentX+1][CurrentY+1] == 0:
+            return(True)
+  return(False)
 
 def findMoves(CurrentX,CurrentY):
-  UpLeft = ValidateMove(CurrentX,CurrentY,'UpLeft')
-  UpRight = ValidateMove(CurrentX,CurrentY,'UpRight')
-  DownLeft = ValidateMove(CurrentX,CurrentY,'DownLeft')
-  DownRight = ValidateMove(CurrentX,CurrentY,'DownRight')
-
-  ValidMove = False
+  if CurrentX > 9 or CurrentY > 9:
+    print('Coordintes out of range')
+    return False, False, False, False
+  else:
+    UpLeft = ValidateMove(CurrentX,CurrentY,'UpLeft')
+    UpRight = ValidateMove(CurrentX,CurrentY,'UpRight')
+    DownLeft = ValidateMove(CurrentX,CurrentY,'DownLeft')
+    DownRight = ValidateMove(CurrentX,CurrentY,'DownRight')
   
-  while ValidMove == False:
-    print('You can move')
-    if UpLeft:
-      print('Up Left (UL)')
-    if UpRight:
-      print('Up Right (UR)')
-    if DownLeft:
-      print('Down Left (DL)')
-    if DownRight:
-      print('Down Right (DR)')
-    Move = input('Type your Move Here: ')
-
-    if Move == 'UL' and not UpLeft:
-      print('Invalid Choice, Cant Move there.')
-    elif Move == 'UR' and not UpRight:
-      print('Invalid Choice, Cant Move there.')
-    elif Move == 'DL' and not DownLeft:
-      print('Invalid Choice, Cant Move there.')
-    elif Move == 'DR'and not DownLeft:
-      print('Invalid Choice, Cant Move there.')
-    elif Move not in ['UL','UR','DL','DR']:
-      print('Invlaid Input')
-    else:
-      MovePiece(CurrentX, CurrentY, Move)
-      ValidMove = True
-      
+    ValidMove = False
+    NoMove = False
+    
+    while ValidMove == False and NoMove == False:
+      if UpLeft:
+        print('Up Left (UL)')
+      if UpRight:
+        print('Up Right (UR)')
+      if DownLeft:
+        print('Down Left (DL)')
+      if DownRight:
+        print('Down Right (DR)')
+      if not UpLeft and not UpRight and not DownLeft and not DownRight:
+        print('This peice can not move')
+        NoMove = True
+        
+      else:
+        Move = input('Type your Move Here: ')
   
-  
-  return UpLeft, UpRight, DownLeft, DownRight
+        if Move == 'UL' and not UpLeft:
+          print('Invalid Choice, Cant Move there.')
+        elif Move == 'UR' and not UpRight:
+          print('Invalid Choice, Cant Move there.')
+        elif Move == 'DL' and not DownLeft:
+          print('Invalid Choice, Cant Move there.')
+        elif Move == 'DR'and not DownLeft:
+          print('Invalid Choice, Cant Move there.')
+        elif Move not in ['UL','UR','DL','DR']:
+          print('Invlaid Input')
+        else:
+          MovePiece(CurrentX, CurrentY, Move)
+          ValidMove = True
+        
+    
+    
+    return UpLeft, UpRight, DownLeft, DownRight

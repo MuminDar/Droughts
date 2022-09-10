ff43a42931a90af12abd4770d8c8c22966f54fa8
Mumin Dar <108408004+MuminDar@users.noreply.github.com>
2022-09-10 21:22:07 +0000

    Working AI (Basic Randomisation)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from random import randint
+from time import sleep
 board = [[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0]]
 
 # code later for counting black white pieces on load board
@@ -39,6 +41,8 @@ class Board:
       BoardFile.write(row + "\n")
 
   def LoadSave(self):
+    Game_OBJ.PieceCountW = 0
+    Game_OBJ.PieceCountB = 0
     row = 0
     BoardFile = open("board.txt","r")
     for line in BoardFile:
@@ -51,16 +55,18 @@ class Board:
             board[row][column] = self.piece(char.lower(),False)
           else:
             board[row][column] = self.piece(char.lower(),True)
+          if char.lower() == 'w':
+            Game_OBJ.PieceCountW += 1
+          elif char.lower() == 'b':
+            Game_OBJ.PieceCountB += 1
         column += 1
       row += 1
     GUI.displayBoard(GUI_OBJ)
+    GUI.displayScores(GUI_OBJ)
 
 class Controller:
   def __init__(self):
     self.MorT = ""
-    self.PieceCountW = 20
-    self.PieceCountB = 20
-    self.turn = "w"
     self.multimove = False
 
   def MovePiece(self,CurrentX,CurrentY,Direction):
@@ -95,16 +101,14 @@ class Controller:
     
     if (not DL and not DR and not UL and not UR) or self.MorT != 'T':
       GUI.displayButtons(GUI_OBJ,False,False,False,False)
-      if self.turn == "w":
-        self.turn = "b"
-        GUI.DisplayTurn(GUI_OBJ, 'Black')
-      elif self.turn == "b":
-        self.turn = "w"
-        GUI.DisplayTurn(GUI_OBJ, 'White')
-      GUI.MorT_Switch(GUI_OBJ)
       if self.multimove == True:
         GUI.displayBoard(GUI_OBJ)
         self.multimove = False
+      if Game_OBJ.turn == "w":
+        Game_OBJ.turnChange()
+      elif Game_OBJ.turn == "b":
+        Game_OBJ.turnChange()
+      GUI.MorT_Switch(GUI_OBJ)
     else:
       GUI.updateXY(GUI_OBJ, CurrentX+x, CurrentY+y)
       GUI.displayButtons(GUI_OBJ,UL,UR,DL,DR)
@@ -114,14 +118,14 @@ class Controller:
       GUI_OBJ.is_on = True
       self.multimove = True
     
-    self.WinCheck()
+    Game_OBJ.WinCheck()
       
 
   def ValidateMove(self,CurrentX,CurrentY,Direction):
     valid = False 
     if board[CurrentX][CurrentY] == 0:
       valid = False 
-    elif board[CurrentX][CurrentY].PieceColour.lower() != self.turn:
+    elif board[CurrentX][CurrentY].PieceColour.lower() != Game_OBJ.turn:
       valid = False
     else:
       if self.MorT == "T": 
@@ -141,7 +145,6 @@ class Controller:
   
   def findMoves(self,CurrentX,CurrentY):
     if CurrentX > 9 or CurrentY > 9:
-      print("Coordintes out of range")
       return False
     else:
       UpLeft = self.ValidateMove(CurrentX,CurrentY,"UL")
@@ -193,22 +196,17 @@ class Controller:
     elif Direction == "DR":
       board[CurrentX+1][CurrentY+1] = 0
     
-    if self.turn == "w":
-      self.PieceCountB -= 1 
+    if Game_OBJ.turn == "w":
+      Game_OBJ.PieceCountB -= 1 
     else:
-      self.PieceCountW -= 1 
+      Game_OBJ.PieceCountW -= 1 
+    GUI_OBJ.displayScores()
 
   def QueenCheck(self,CurrentX, CurrentY):
     if board[CurrentX][CurrentY].PieceColour == "w" and CurrentX == 9: 
       board[CurrentX][CurrentY].queen = True 
     elif board[CurrentX][CurrentY].PieceColour == "b" and CurrentX == 0: 
       board[CurrentX][CurrentY].queen = True
-
-  def WinCheck(self):
-    if self.PieceCountB == 0:
-      GUI.WinScreen(GUI_OBJ, 'White')
-    elif self.PieceCountW == 0:
-      GUI.WinScreen(GUI_OBJ, 'Black')
 
 class GUI:
   def __init__(self):
@@ -261,19 +259,19 @@ class GUI:
   
   def displayButtons(self, UpLeft, UpRight, DownLeft, DownRight):
     if UpLeft:
-      tk.Button(self.canvas, bg= 'green', text= 'UL', command=lambda:Controller.MovePiece(Controller_OBJ,self.ROW, self.COLUMN, 'UL')).grid(row=4,column=12)
+      tk.Button(self.canvas, bg= 'green', text= 'UL', command=lambda:Controller_OBJ.MovePiece(self.ROW, self.COLUMN, 'UL')).grid(row=4,column=12)
     else:
       tk.Button(self.canvas, bg= 'grey', text= 'UL', state= tk.DISABLED).grid(row=4,column=12)
     if UpRight:
-      tk.Button(self.canvas, bg= 'green', text= 'UR', command=lambda:Controller.MovePiece(Controller_OBJ,self.ROW, self.COLUMN, 'UR')).grid(row=4,column=14)
+      tk.Button(self.canvas, bg= 'green', text= 'UR', command=lambda:Controller_OBJ.MovePiece(self.ROW, self.COLUMN, 'UR')).grid(row=4,column=14)
     else:
       tk.Button(self.canvas, bg= 'grey', text= 'UR', state= tk.DISABLED).grid(row=4,column=14)
     if DownLeft:
-      tk.Button(self.canvas, bg= 'green', text= 'DL', command=lambda:Controller.MovePiece(Controller_OBJ,self.ROW, self.COLUMN, 'DL')).grid(row=6,column=12)
+      tk.Button(self.canvas, bg= 'green', text= 'DL', command=lambda:Controller_OBJ.MovePiece(self.ROW, self.COLUMN, 'DL')).grid(row=6,column=12)
     else:
       tk.Button(self.canvas, bg= 'grey', text= 'DL', state= tk.DISABLED).grid(row=6,column=12)
     if DownRight:
-      tk.Button(self.canvas, bg= 'green', text= 'DR', command=lambda:Controller.MovePiece(Controller_OBJ,self.ROW, self.COLUMN, 'DR')).grid(row=6,column=14)
+      tk.Button(self.canvas, bg= 'green', text= 'DR', command=lambda:Controller_OBJ.MovePiece(self.ROW, self.COLUMN, 'DR')).grid(row=6,column=14)
     else:
       tk.Button(self.canvas, bg= 'grey', text= 'DR', state= tk.DISABLED).grid(row=6,column=14)
 
@@ -329,24 +327,207 @@ class GUI:
 
   def ClearScreen(self):
     self.canvas.destroy()
-    print('die')
+
+  def displayScores(self):
+    tk.Label(self.canvas, text=f'W : {Game_OBJ.PieceCountW} ').grid(row=0,column=13)
+    tk.Label(self.canvas, text=f'B : {Game_OBJ.PieceCountB} ').grid(row=1,column=13)
   
   def WinScreen(self, winner):
     self.ClearScreen()
     self.canvas = tk.Canvas(self.window, width=600, height=400)
     self.canvas.grid()
-    tk.Label(self.canvas, text=f'{winner} is the Winner !!!', font=("Times New Roman", 25)).grid(row=1,column=15, padx = 50, pady = 150)
+    tk.Label(self.canvas, text=f'{winner} is the Winner !!!', font=("Times New Roman", 25)).grid(row=1,column=1, padx = 50, pady = 150)
 
-Board_OBJ = Board()
-GUI_OBJ = GUI()
-Controller_OBJ = Controller()
 
-GUI.DisplayTurn(GUI_OBJ, 'White')
+class AI(Controller):
+  def __init__(self, colour):
+    Controller.__init__(self)
+    self.HighestPoints = 0
+    self.BestX = []
+    self.BestY = []
+    self.BestMove = []
+    self.moves = ['UL', 'UR', 'DL', 'DR']
+    self.bestMorT = 'M'
+    self.colour = colour
 
-GUI_OBJ.displayBoard()
-GUI_OBJ.displayButtons(False,False,False,False)
-GUI_OBJ.SaveLoadButtons()
-GUI_OBJ.MorT_Switch()
+  def MovePiece(self,CurrentX,CurrentY,Direction):
+    x = 0
+    y = 0
+    if Direction[0] == "D":
+      x = 1
+    else:
+      x = -1
+    if Direction[1] == "R":
+      y = 1
+    else: 
+      y = -1 
+    if self.MorT == "T" :
+      x = x*2
+      y = y*2  
+      self.TakePiece(CurrentX,CurrentY,Direction)
+    temp = board[CurrentX][CurrentY]
+    board[CurrentX+x][CurrentY+y] = temp
+    self.QueenCheck(CurrentX+x,CurrentY+y)
+    board[CurrentX][CurrentY] = 0
+    #updating only the tiles changed
+    GUI.updateTile(GUI_OBJ, CurrentX, CurrentY)
+    GUI.updateTile(GUI_OBJ, CurrentX+x, CurrentY+y)
+    if self.MorT == 'T':
+      GUI.updateTile(GUI_OBJ, CurrentX+int(x/2), CurrentY+int(y/2))
 
-tk.mainloop()
+    UL = self.CheckTakePossible(CurrentX+x,CurrentY+y, 'UL')
+    UR = self.CheckTakePossible(CurrentX+x,CurrentY+y, 'UR')
+    DL = self.CheckTakePossible(CurrentX+x,CurrentY+y, 'DL')
+    DR = self.CheckTakePossible(CurrentX+x,CurrentY+y, 'DR')
+    
+    if (not DL and not DR and not UL and not UR) or self.MorT != 'T':
+      GUI.displayButtons(GUI_OBJ,False,False,False,False)
+      if Game_OBJ.turn == "w":
+        Game_OBJ.turnChange()
+      elif Game_OBJ.turn == "b":
+        Game_OBJ.turnChange()
+      GUI.MorT_Switch(GUI_OBJ)
+    else:
+      possibleMoves = []
+      if UL:
+        possibleMoves.append('UL')
+      if UR:
+        possibleMoves.append('UR')
+      if DL:
+        possibleMoves.append('DL')
+      if DR:
+        possibleMoves.append('DR')
+      rand = randint(0,(len(possibleMoves)-1))
+      move = possibleMoves[rand]
+      self.MovePiece(CurrentX+x,CurrentY+y,move)
+      
+    Game_OBJ.WinCheck()
+
+  
+  def MakeMove(self):
+    self.BestX = []
+    self.BestY = []
+    self.BestMove = []
+    self.moves = ['UL', 'UR', 'DL', 'DR']
+    self.bestMorT = 'M'
+    if self.colour == Game_OBJ.turn:
+      for row in range(10):
+        for column in range(10):
+          if board[row][column] != 0:
+            if board[row][column].PieceColour == Game_OBJ.turn:
+              for move in self.moves:
+                self.MorT = 'M'
+                if self.ValidateMove(row,column,move) == True and self.bestMorT == 'M':
+                  if self.bestMorT == 'M':
+                    self.BestX.append(row)
+                    self.BestY.append(column)
+                    self.BestMove.append(move)
+                else:
+                  self.MorT = 'T'
+                  if self.ValidateMove(row,column,move) == True:
+                    if self.bestMorT == 'M':
+                      self.BestX = [row]
+                      self.BestY = [column]
+                      self.BestMove = [move]
+                      self.bestMorT = 'T'
+                    else:
+                      self.BestX.append(row)
+                      self.BestY.append(column)
+                      self.BestMove.append(move)
+                    
+  
+      #randomize if there are multiple possible moves
+      rand = randint(0, (len(self.BestX)-1))
+      self.BestX = self.BestX[rand]
+      self.BestY = self.BestY[rand]
+      self.BestMove = self.BestMove[rand]
+      
+      self.MorT = self.bestMorT
+      self.MovePiece(self.BestX, self.BestY, self.BestMove)
+        
+
+
+class Game:
+  def __init__(self):
+    self.PieceCountW = 20
+    self.PieceCountB = 20
+    self.turn = "w"
+    self.AICount = 0
+
+  def WinCheck(self):
+    if Game_OBJ.PieceCountB == 0:
+      GUI.WinScreen(GUI_OBJ, 'White')
+    elif Game_OBJ.PieceCountW == 0:
+      GUI.WinScreen(GUI_OBJ, 'Black')
+
+  def turnChange(self):
+    if self.turn == "w":
+      self.turn = "b"
+      GUI.DisplayTurn(GUI_OBJ, 'Black')
+    elif self.turn == "b":
+      self.turn = "w"
+      GUI.DisplayTurn(GUI_OBJ, 'White')
+    if self.AICount > 0:
+      AI_OBJ.MakeMove()
+    if self.AICount > 1:
+      AI_OBJ2.MakeMove()
+
+  def setup(self):
+    GUI_OBJ.DisplayTurn('White')
+    GUI_OBJ.displayBoard()
+    GUI_OBJ.displayScores()
+    GUI_OBJ.displayButtons(False,False,False,False)
+    GUI_OBJ.SaveLoadButtons()
+    GUI_OBJ.MorT_Switch()
+
+  def PVP(self):
+    global Board_OBJ
+    global GUI_OBJ
+    global Controller_OBJ
+    Board_OBJ = Board()
+    GUI_OBJ = GUI()
+    Controller_OBJ = Controller()
+    
+    self.setup()
+    
+    tk.mainloop()
+
+  def PVE(self):
+    global Board_OBJ
+    global GUI_OBJ
+    global Controller_OBJ
+    global AI_OBJ
+    Board_OBJ = Board()
+    GUI_OBJ = GUI()
+    Controller_OBJ = Controller()
+    AI_OBJ = AI('b')
+    self.AICount = 1
+    
+    self.setup()
+
+    tk.mainloop()
+
+  def EVE(self):
+    global Board_OBJ
+    global GUI_OBJ
+    global Controller_OBJ
+    global AI_OBJ
+    global AI_OBJ2
+    Board_OBJ = Board()
+    GUI_OBJ = GUI()
+    Controller_OBJ = Controller()
+    AI_OBJ = AI('b')
+    AI_OBJ2 = AI('w')
+
+    self.AICount = 2
+    
+    AI_OBJ2.MakeMove()
+    
+    self.setup()
+
+    tk.mainloop()
+
+Game_OBJ = Game()
+
+Game_OBJ.PVE()
 #commented
